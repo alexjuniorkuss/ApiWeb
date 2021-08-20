7468b336a0badf4d6b41d5c0523fef3921acad6d
Alex Junior <alexjuniorkuss@outlook.com>
2021-08-20 09:00:37 -0300

    Created clone of repository and done somethings

---
+++ Data/Context/CrudContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class CrudContext : DbContext
+    {
+        public CrudContext() : base("Data Source=192.168.0.161;Initial Catalog=MVCAlex;User ID=Adilson;Password=123321@")
+        {
+        }
+    }
+}
---
+++ Data/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Teacher : BaseModel
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Subject { get; set; }
+    }
+}
